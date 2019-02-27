ffd065838f75c7e016786ca41559f319960d90e7
Justin Huey <juhuey1@s.sfusd.edu>
2019-02-27 01:50:54 +0000

    Start

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
+  $.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    method: "GET",
+    success: function(response){
+    $(".imagePlace").append("<img src=" + response.data[0].images.original.url + ">");
+        
+    }
+      
+      
+ 
   
-  
-  
+
+  });
 });
 
