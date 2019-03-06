f4a202d85ce2389270e842a1b6981980989bd30f
Justin Huey <juhuey1@s.sfusd.edu>
2019-03-06 02:03:15 +0000

    unique API

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 $("#search-button").click(function(){
+  var input = $("#search-term").val();
+  $("#search-term").val("");
   $.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    url: `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`,
     method: "GET",
     success: function(response){
     $(".imagePlace").append("<img src=" + response.data[0].images.original.url + ">");
         
     }
-      
-      
- 
-  
-
   });
+});
+
+$("#clear").click(function(){
+  $(".imagePlace").html("");
+  $("#search-term").val("");
 });
 
