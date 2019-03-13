f4e0f0c9322c4acb85c56ab3b95ab56c697e888c
Justin Huey <juhuey1@s.sfusd.edu>
2019-03-13 00:55:26 +0000

    Extensions

--- js/script.js
+++ js/script.js
@@ -4,12 +4,29 @@
 /* global $ */
 $("#search-button").click(function(){
   var input = $("#search-term").val();
-  $("#search-term").val("");
+  
   $.ajax({
     url: `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`,
     method: "GET",
     success: function(response){
-    $(".imagePlace").append("<img src=" + response.data[0].images.original.url + ">");
+    for(var i = 0; i < response.data.length; i++){
+      $(".imagePlace").append("<img src=" + response.data[i].images.original.url + ">");
+        
+      }
+    }
+  });
+});
+
+$("#random").click(function(){
+  var input = $("#search-term").val();
+  
+  $.ajax({
+    url: `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`,
+    method: "GET",
+    success: function(response){
+    var images = response.data.length;
+    var num = Math.floor(Math.random()* images);
+    $(".imagePlace").append("<img src=" + response.data[num].images.original.url + ">");
         
     }
   });
@@ -19,4 +36,3 @@ $("#clear").click(function(){
   $(".imagePlace").html("");
   $("#search-term").val("");
 });
-
