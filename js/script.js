1befeb8989d1ebb5d59acb72a5c213c2bdf4a511
Justin Huey <juhuey1@s.sfusd.edu>
2019-03-19 23:28:42 +0000

    Cleaned up some code

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+ 
 $("#search-button").click(function(){
   var input = $("#search-term").val();
-  
+  var url = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`;
   $.ajax({
-    url: `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`,
+    url: url,
     method: "GET",
     success: function(response){
-    for(var i = 0; i < response.data.length; i++){
-      $(".imagePlace").append("<img src=" + response.data[i].images.original.url + ">");
+      for(var i = 0; i < response.data.length; i++){
+        $(".imagePlace").append("<img src=" + response.data[i].images.original.url + ">");
         
       }
     }
@@ -19,16 +20,16 @@ $("#search-button").click(function(){
 
 $("#random").click(function(){
   var input = $("#search-term").val();
-  
+  var url = `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`;
   $.ajax({
-    url: `https://api.giphy.com/v1/gifs/search?q=${input}&rating=pg&api_key=dc6zaTOxFJmzC&random`,
+    url: url,
     method: "GET",
     success: function(response){
-    var images = response.data.length;
-    var num = Math.floor(Math.random()* images);
-    $(".imagePlace").append("<img src=" + response.data[num].images.original.url + ">");
+      var images = response.data.length;
+      var num = Math.floor(Math.random()* images);
+      $(".imagePlace").append("<img src=" + response.data[num].images.original.url + ">");
         
-    }
+      }
   });
 });
 
